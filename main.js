dc729cb1d1249031a2f33aa8541e5aff48e42795
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-03-11 22:48:19 +0200

    Separating logic into different views.
    On page load user is greeted with the fetched profiles. When the user presses the see posts anchor a new view is loaded.

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 // Fetches data from the provided url argument.
-async function getData(url) {
+async function getUsers(url) {
     const fetchData = await fetch(url);
     const dataSet = await fetchData.json();
     dataSet.forEach(data => {
         createUserElement(data);
-        
     });
 }
 
-getData('https://jsonplaceholder.typicode.com/users');
+getUsers('https://jsonplaceholder.typicode.com/users');
 
 // Creates the user card and appends it to the view.
 function createUserElement(userData) {
-    const dataView = document.querySelector('.data-view');
+    const dataView = document.querySelector('.user-view');
     
     const userCard = document.createElement('li');
     const userInfoContainer = document.createElement('div');
@@ -30,41 +29,15 @@ function createUserElement(userData) {
     userInfoContainer.append(userName, userRealName, userEmail);
 
     // See selected users post history.
-    const seePostsBtn = document.createElement('button');
-    seePostsBtn.type = 'button';
-    seePostsBtn.textContent = 'See posts';
+    const seePostsAnchor = document.createElement('a');
+    seePostsAnchor.href = './views/userprofile.html';
+    seePostsAnchor.textContent = 'See posts';
    
-    seePostsBtn.addEventListener('click', async () => {
-        clearView(dataView);
-        const fetchData = await fetch ('https://jsonplaceholder.typicode.com/posts');
-        let dataSet = await fetchData.json();
-        dataSet = [...dataSet];
-        const userPosts = dataSet.filter(posts => posts.userId === userData.id);
-        console.log(userPosts);
-        createPostHistoryView(userPosts, userData.username);
-
+    seePostsAnchor.addEventListener('click', () => {
+        sessionStorage.setItem('user', `${userData.username}`);
     })
-    userCard.append(userInfoContainer,seePostsBtn);
+    userCard.append(userInfoContainer,seePostsAnchor);
     dataView.append(userCard);
 }
 
-function clearView(view) {
-    view.innerHTML = '';
-}
 
-function createPostHistoryView(userPostHistory, userName) {
-    userPostHistory.forEach(post => {
-        const dataView = document.querySelector('.data-view');
-    
-        const username = document.createElement('h3');
-        username.textContent = userName;
-    
-        const title = document.createElement('h2');
-        title.textContent = post.title;
-    
-        const body = document.createElement('p');
-        body.textContent = post.body;
-        
-        dataView.append(username, title, body);
-    })
-}
