4f5ba15f365107f1a7b061834e788966a0afb703
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-03-12 01:39:21 +0200

    Added CreateFullUserProfileElement func
    Using the session storage load the users posts and users information and
    append them to the view.

--- views/userprofile/userProfile.js
+++ views/userprofile/userProfile.js
@@ -1,6 +1,7 @@
 function createPostHistoryView(userPostHistory) {
+    const postHistoryView = document.querySelector('.post-history');
+    
     userPostHistory.forEach(post => {
-        const postHistoryView = document.querySelector('.post-history');
         const postCardItem = document.createElement('li');
 
         const postInfoContainer = document.createElement('div');
@@ -35,10 +36,34 @@ async function getUserInformation(url) {
     users = [...users];
     const userId = Number(sessionStorage.getItem('userId'));
     const userProfile = users.filter(user => user.id === userId);
-    console.log(userProfile);
-
+    createFullUserProfileElement(userProfile);
+    console.table(userProfile);
 }
 
 getUserInformation('https://jsonplaceholder.typicode.com/users')
 
-getPosts('https://jsonplaceholder.typicode.com/posts');
+getPosts('https://jsonplaceholder.typicode.com/posts')
+
+function createFullUserProfileElement(userData) {
+    const userInfoContainer = document.querySelector('.user-info-container');
+
+    const fullname = document.createElement('h2');
+    fullname.textContent = userData[0].name;
+
+    const username = document.createElement('h3');
+    username.textContent = `@${userData[0].username}`;
+
+    const email = document.createElement('h4');
+    email.textContent = userData[0].email;
+
+    const phone = document.createElement('h4');
+    phone.textContent = userData[0].phone;
+
+    const website = document.createElement('a');
+    website.href = '';
+    website.textContent = `www.${userData[0].website}`;
+
+
+    userInfoContainer.append(fullname, username, email, phone, website);
+
+}
