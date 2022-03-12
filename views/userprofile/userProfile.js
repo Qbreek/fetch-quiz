07b1e1f6ea9c2e55abdc345d3662c74086753f48
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-03-12 21:17:29 +0200

    Pseudo form working, added delete and edit btns
    TODO : Delete and edit btn functionality

--- views/userprofile/userProfile.js
+++ views/userprofile/userProfile.js
@@ -1,47 +1,77 @@
-function createPostElementCard(userPostHistory) {
-    const postHistoryView = document.querySelector('.post-history-list');
-    
-    userPostHistory.forEach(post => {
-        const postCardItem = document.createElement('li');
-        postCardItem.classList.add('post-item-card');
-    
-        const title = document.createElement('h2');
-        title.textContent = post.title;
-    
-        const body = document.createElement('p');
-        body.textContent = post.body;
-
-        postCardItem.append(title, body);
-        
-        postHistoryView.append(postCardItem);
-    })
-}
-
-// Fetches data from the provided url argument.
+// Fetches users post history from the provided url argument using the value stored in session storage.
 async function getPosts(url) {
+   
     const postData = await fetch(url);
     let posts = await postData.json();
     posts = [...posts];
     const userId = Number(sessionStorage.getItem('userId'));
     const userPosts = posts.filter(post => post.userId === userId);
-    createPostElementCard(userPosts)
+    createPostElementCard(userPosts);
 }
 
+// Fetches user information based on the value stored in session storage.
 async function getUserInformation(url) {
+    
     const userData = await fetch(url);
     let users = await userData.json();
     users = [...users];
     const userId = Number(sessionStorage.getItem('userId'));
     const userProfile = users.filter(user => user.id === userId);
     createFullUserProfileElement(userProfile);
-    console.table(userProfile);
 }
 
-getUserInformation('https://jsonplaceholder.typicode.com/users')
+// Creates all the post-card elements and appends them to post history.
+function createPostElementCard(userPostHistory) {
+    
+    const postHistoryView = document.querySelector('.post-history-list');
+    
+    userPostHistory.forEach(post => {
+        const postCardItem = document.createElement('li');
+        postCardItem.classList.add('post-item-card');
+        postCardItem.id = `${post.id}`;
 
-getPosts('https://jsonplaceholder.typicode.com/posts')
+        const titleAndButtonContainer = document.createElement('div');
+        titleAndButtonContainer.classList.add('title-and-buttons-container');
 
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.classList.add('buttons-container-postcard');
+        
+        const title = document.createElement('h2');
+        title.textContent = post.title;
+        
+        const editBtn = document.createElement('button');
+        editBtn.classList.add('post-card-btn');
+
+        const embedEditSvg = document.createElement('embed');
+        embedEditSvg.src = '/svg/edit_black_24dp.svg';
+
+        editBtn.append(embedEditSvg);
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('post-card-btn');
+
+        const embedDeleteSvg = document.createElement('embed');
+        embedDeleteSvg.classList.add()
+        embedDeleteSvg.src = '/svg/delete_black_24dp.svg';
+
+        deleteBtn.append(embedDeleteSvg);
+
+        buttonsContainer.append(editBtn, deleteBtn);
+
+        titleAndButtonContainer.append(title, buttonsContainer);
+    
+        const body = document.createElement('p');
+        body.textContent = post.body;
+
+        postCardItem.append(titleAndButtonContainer, body);
+        
+        postHistoryView.append(postCardItem);
+    })
+}
+
+// Creates full user profile using session storage value.
 function createFullUserProfileElement(userData) {
+   
     const userInfoContainer = document.querySelector('.user-full-profile-card');
 
     const profileSvg = document.createElement('embed');
@@ -65,3 +95,47 @@ function createFullUserProfileElement(userData) {
 
     userInfoContainer.append(profileSvg, fullname, username, email, phone, website);
 }
+
+
+function main() {
+    getUserInformation('https://jsonplaceholder.typicode.com/users');
+    getPosts('https://jsonplaceholder.typicode.com/posts');
+
+    const pseudoSubmitBtn = document.querySelector('.pseudo-submit');
+    
+    // Simulates submit procedure? Maybe im wrong here
+    pseudoSubmitBtn.addEventListener('click', () => {
+       
+        const titleInput = document.querySelector('input[name="title"]');
+        const bodyInput = document.querySelector('textarea[name="body"]');
+       
+        const postObject = {
+            title: titleInput.value,
+            body: bodyInput.value,
+            userId: Number(sessionStorage.getItem('userId'))
+        };
+
+        if (titleInput.value != '' && bodyInput.value != '') {
+            
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify(postObject),
+                headers: {
+                  'Content-type': 'application/json; charset=UTF-8',
+                },
+              })
+            .then((response) => response.json())
+            .then((json) => console.log(json))
+            .then(() => alert('Check Console'))
+
+            title.value = '';
+            bodyInput.value = '';
+       
+        } else {
+            
+            alert('Please fill out all the input fields');
+        }
+    });
+}
+
+main();
