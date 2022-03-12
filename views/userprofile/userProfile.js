851fa51f5dff4fedbc15233435faeeea506a7456
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-03-12 17:53:58 +0200

    Refactoring code while cleaning CSS file

--- views/userprofile/userProfile.js
+++ views/userprofile/userProfile.js
@@ -1,11 +1,9 @@
-function createPostHistoryView(userPostHistory) {
-    const postHistoryView = document.querySelector('.post-history');
+function createPostElementCard(userPostHistory) {
+    const postHistoryView = document.querySelector('.post-history-list');
     
     userPostHistory.forEach(post => {
         const postCardItem = document.createElement('li');
-
-        const postInfoContainer = document.createElement('div');
-        postInfoContainer.classList.add('post-item');
+        postCardItem.classList.add('post-item-card');
     
         const title = document.createElement('h2');
         title.textContent = post.title;
@@ -13,8 +11,7 @@ function createPostHistoryView(userPostHistory) {
         const body = document.createElement('p');
         body.textContent = post.body;
 
-        postInfoContainer.append(title, body);
-        postCardItem.append(postInfoContainer);
+        postCardItem.append(title, body);
         
         postHistoryView.append(postCardItem);
     })
@@ -27,7 +24,7 @@ async function getPosts(url) {
     posts = [...posts];
     const userId = Number(sessionStorage.getItem('userId'));
     const userPosts = posts.filter(post => post.userId === userId);
-    createPostHistoryView(userPosts)
+    createPostElementCard(userPosts)
 }
 
 async function getUserInformation(url) {
@@ -45,7 +42,10 @@ getUserInformation('https://jsonplaceholder.typicode.com/users')
 getPosts('https://jsonplaceholder.typicode.com/posts')
 
 function createFullUserProfileElement(userData) {
-    const userInfoContainer = document.querySelector('.user-info-container');
+    const userInfoContainer = document.querySelector('.user-full-profile-card');
+
+    const profileSvg = document.createElement('embed');
+    profileSvg.src = '/svg/account_circle_black_48dp.svg';
 
     const fullname = document.createElement('h2');
     fullname.textContent = userData[0].name;
@@ -53,17 +53,15 @@ function createFullUserProfileElement(userData) {
     const username = document.createElement('h3');
     username.textContent = `@${userData[0].username}`;
 
-    const email = document.createElement('h4');
+    const email = document.createElement('h5');
     email.textContent = userData[0].email;
 
-    const phone = document.createElement('h4');
+    const phone = document.createElement('h5');
     phone.textContent = userData[0].phone;
 
     const website = document.createElement('a');
     website.href = '';
     website.textContent = `www.${userData[0].website}`;
 
-
-    userInfoContainer.append(fullname, username, email, phone, website);
-
+    userInfoContainer.append(profileSvg, fullname, username, email, phone, website);
 }
