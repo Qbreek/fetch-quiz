de35522adde62589a359b0c031e9d340c83c2a21
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-03-13 17:00:25 +0200

    Made code modular.
    Moved all functions to different files into module folder.

--- index.js
+++ index.js
@@ -1,54 +1,10 @@
-// Fetches data from the provided url argument.
-async function getUsers(url) {
-    
-    const userData = await fetch(url);
-    const users = await userData.json();
-    users.forEach(user => {
-        createUserProfileCardElement(user);
-    });
-}
+import getUsers from "./modules/getUsers.js";
 
-// Creates the user profile card and appends it to the view.
-function createUserProfileCardElement(userData) {
-    
-    const userList = document.querySelector('.user-profile-list');
-    
-    const userCard = document.createElement('li');
-    userCard.classList.add('user-profile-card-index');
-
-    const embedProfileSvg = document.createElement('embed');
-    embedProfileSvg.src = './svg/account_circle_black_36dp.svg'
-
-    userCard.append(embedProfileSvg)
-
-    const userInfoContainer = document.createElement('div');
-    userInfoContainer.classList.add('user-info-wrapper');
-    
-    const userName = document.createElement('h3');
-    userName.textContent = userData.username;
-    
-    const userRealName = document.createElement('h5');
-    userRealName.textContent = userData.name;
-
-    userInfoContainer.append(userName, userRealName);
-
-    // See selected users post history.
-    const seeProfileAnchor = document.createElement('a');
-    seeProfileAnchor.href = '/fetch-quiz/views/userprofile/userprofile.html';
-    seeProfileAnchor.textContent = 'See profile';
-    
-    seeProfileAnchor.addEventListener('click', async () => {
-        sessionStorage.setItem('userId', `${userData.id}`);
-    })
-    userCard.append(userInfoContainer,seeProfileAnchor);
-    userList.append(userCard);
-}
-
-function main() {
+function index() {
     
     getUsers('https://jsonplaceholder.typicode.com/users');
 }
 
-main();
+index();
 
 
